eb2ca8a8fd91332a75fdc9c6f8e7da13b561ae87
Faiz Khan <faizkhan00@gmail.com>
2017-07-10 00:02:07 -0500

    add shader (funsies)

--- core/src/scala/com/mygdx/game/screen/TiledScreen.scala
+++ core/src/scala/com/mygdx/game/screen/TiledScreen.scala
@@ -11,6 +11,12 @@ import com.badlogic.gdx.math.Rectangle;
 import com.mygdx.game.GameApplication
 import com.mygdx.game._;
 
+import com.badlogic.gdx.graphics.glutils.ShaderProgram;
+import com.badlogic.gdx.graphics.Mesh;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.VertexAttribute;
+import com.badlogic.gdx.math.Matrix4;
+
 class TiledScreen(game: GameApplication) extends ScreenAdapter{
 
   val level = "levels/level1.tmx";
@@ -43,7 +49,47 @@ class TiledScreen(game: GameApplication) extends ScreenAdapter{
   purp.setPosition(80.0f, 150.0f);
 
   Gdx.input.setInputProcessor(PlayerInput.defaultAdapter(purp));
+
+  //disable for now
+  val enableNextGenAnims = false;
   val tp = new TestPlayer(atlas);
+
+  //shaders
+  val vertexShader = "attribute vec4 a_position;    \n" + 
+                      "attribute vec4 a_color;\n" +
+                      "attribute vec2 a_texCoord0;\n" + 
+                      "uniform mat4 u_projTrans;\n" + 
+                      "varying vec4 v_color;" + 
+                      "varying vec2 v_texCoords;" + 
+                      "void main()                  \n" + 
+                      "{                            \n" + 
+                      "   v_color = vec4(1, 1, 1, 1); \n" + 
+                      "   v_texCoords = a_texCoord0; \n" + 
+                      "   gl_Position =  u_projTrans * a_position;  \n"      + 
+                      "}                            \n" ;
+
+  val fragmentShader = "#ifdef GL_ES\n" +
+                        "precision mediump float;\n" + 
+                        "#endif\n" + 
+                        "varying vec4 v_color;\n" + 
+                        "varying vec2 v_texCoords;\n" + 
+                        "uniform sampler2D u_texture;\n" + 
+                        "void main()                                  \n" + 
+                        "{                                            \n" + 
+                        "  gl_FragColor = v_color * texture2D(u_texture, v_texCoords);\n" +
+                        "}";
+
+  var shader = new ShaderProgram(vertexShader, fragmentShader);
+
+  var mesh = new Mesh(true, 4, 6, VertexAttribute.Position(), VertexAttribute.ColorUnpacked(), VertexAttribute.TexCoords(0));
+  mesh.setVertices(Array[Float](-0.5f, -0.5f, 0, 1, 1, 1, 1, 0, 1,
+  0.5f, -0.5f, 0, 1, 1, 1, 1, 1, 1,
+  0.5f, 0.5f, 0, 1, 1, 1, 1, 1, 0,
+  -0.5f, 0.5f, 0, 1, 1, 1, 1, 0, 0));
+  mesh.setIndices(Array[Short](0, 1, 2, 2, 3, 0));
+  var texture = new Texture(Gdx.files.internal("levels/images/ZsPH5nFNESJyZ1IyEnEuMvKPryyoMeu2LuWu58PxVBo.png")); 
+  var matrix = new Matrix4();
+
 
   def update(dt: Float) {
 
@@ -51,8 +97,9 @@ class TiledScreen(game: GameApplication) extends ScreenAdapter{
       Gdx.app.exit
 
     purp.update(dt);
-    tp.update(dt);
-    
+
+    if (enableNextGenAnims) tp.update(dt);
+
   }
 
   override def render (dt: Float) {
@@ -65,11 +112,20 @@ class TiledScreen(game: GameApplication) extends ScreenAdapter{
     batch.begin();
 
     purp.draw(batch);
-    tp.draw(batch);
+
+    if(enableNextGenAnims) tp.draw(batch);
 
     font.draw(batch, "FPS: " + Gdx.graphics.getFramesPerSecond(), 10, 20);
 
     batch.end();
+
+texture.bind();
+shader.begin();
+shader.setUniformMatrix("u_projTrans", matrix);
+shader.setUniformi("u_texture", 0);
+mesh.render(shader, GL20.GL_TRIANGLES);
+shader.end();
+
   }
 
   override def dispose () {
